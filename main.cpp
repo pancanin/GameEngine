3f66662751ca54a44f4e242b37e95fc8bf13682e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-12-18 16:40:20 -0500

    booting up sdl - wip

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+#include <SDL.h>
+
+SDL_Surface* test = nullptr;
+
+int32_t main([[maybe_unused]]int32_t argc, [[maybe_unused]]char** argv) {
+
+	return EXIT_FAILURE;
+}
